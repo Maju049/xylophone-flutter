a8c22a13a737654b20b947c79a608f9c971b6df8
Maju049 <64065807+Maju049@users.noreply.github.com>
2020-09-25 10:57:36 +0200

    Update main.dart
    input argument in function

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp(
+    XylophoneApp(),
+  );
+}
 
 class XylophoneApp extends StatelessWidget {
+  void clickplay(int soundnum) {
+    final player = AudioCache();
+    player.play('note$soundnum.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueGrey[800],
+                  onPressed: () {
+                    clickplay(1);
+                  },
+                  child: Text('sound1'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueGrey[700],
+                  onPressed: () {
+                    clickplay(2);
+                  },
+                  child: Text('sound2'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueGrey[600],
+                  onPressed: () {
+                    clickplay(3);
+                  },
+                  child: Text('sound3'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueGrey[500],
+                  onPressed: () {
+                    clickplay(4);
+                  },
+                  child: Text('sound4'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueGrey[400],
+                  onPressed: () {
+                    clickplay(5);
+                  },
+                  child: Text('sound5'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueGrey[300],
+                  onPressed: () {
+                    clickplay(6);
+                  },
+                  child: Text('sound6'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
